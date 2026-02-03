ba6eb98eff1277fbd3f546cec1b24a8b3ce70985
William Donnette <willdonn17@gmail.com>
2026-02-03 16:08:58 +0100

    feat: update Dockerfile and entrypoint script for Vue 3 environment variables

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
 # Paramètres par défaut du conteneur
-export LN_VUE_APP_ROOT_API=${LN_VUE_APP_ROOT_API:='https://acces.licencesnationales.fr/v1/api/'}
-export LN_VUE_APP_RECAPTCHA_KEY_SITE=${LN_VUE_APP_RECAPTCHA_KEY_SITE:='6LcjHroaAAAAAOhO6sd_1ooz5LfcyRPoYirqxsFt'}
+export VITE_APP_ROOT_API=${VITE_APP_ROOT_API:='https://acces.licencesnationales.fr/v1/api/'}
+export VITE_APP_RECAPTCHA_KEY_SITE=${VITE_APP_RECAPTCHA_KEY_SITE:='6LcjHroaAAAAAOhO6sd_1ooz5LfcyRPoYirqxsFt'}
+export VITE_APP_VERSION=${VITE_APP_VERSION:='front'}
 
 
 # Remplace les placeholders dans le code généré en prod
 # LN_PLACEHOLDER_VUE_APP_RECAPTCHA_KEY_SITE
 # LN_PLACEHOLDER_VUE_APP_ROOT_API
+# LN_PLACEHOLDER_VERSION
 # On va remplacer les placeholders depuis les JS originales
 echo "-> Remplacement des placeholders venant du .env de vuejs dans la destination /usr/share/nginx/html/"
-echo "-> LN_VUE_APP_ROOT_API=${LN_VUE_APP_ROOT_API}"
-echo "-> LN_VUE_APP_RECAPTCHA_KEY_SITE=${LN_VUE_APP_RECAPTCHA_KEY_SITE}"
+echo "-> VITE_APP_ROOT_API=${VITE_APP_ROOT_API}"
+echo "-> VITE_APP_RECAPTCHA_KEY_SITE=${VITE_APP_RECAPTCHA_KEY_SITE}"
+echo "-> VITE_APP_VERSION=${VITE_APP_VERSION}"
 rm -rf /usr/share/nginx/html/
 cp -rf /usr/share/nginx/html.orig/ /usr/share/nginx/html/
-sed -i "s#LN_PLACEHOLDER_VUE_APP_ROOT_API#${LN_VUE_APP_ROOT_API}#g" /usr/share/nginx/html/js/*
-sed -i "s#LN_PLACEHOLDER_VUE_APP_RECAPTCHA_KEY_SITE#${LN_VUE_APP_RECAPTCHA_KEY_SITE}#g" /usr/share/nginx/html/js/*
+sed -i "s#LN_PLACEHOLDER_VUE_APP_ROOT_API#${VITE_APP_ROOT_API}#g" /usr/share/nginx/html/assets/*
+sed -i "s#LN_PLACEHOLDER_VUE_APP_RECAPTCHA_KEY_SITE#${VITE_APP_RECAPTCHA_KEY_SITE}#g" /usr/share/nginx/html/assets/*
+sed -i "s#LN_PLACEHOLDER_VERSION#${VITE_APP_VERSION}#g" /usr/share/nginx/html/assets/*
 
 
 
